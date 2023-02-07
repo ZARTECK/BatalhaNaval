2ef7621559bed23d71aa72a25028a2a3791f1d0d
zarteck <tomascosta99@gmail.com>
2023-02-07 16:31:45 +0000

    small fixes

--- Program.cs
+++ Program.cs
@@ -302,7 +302,7 @@ namespace Trabalho_Avaliacao
       
             static void InserirBarcos(char[,] board , int Sizetabuleiro)
             {
-                for (int i = 0; i < 2; i++)
+                for (int i = 0; i < 8; i++)
                 {
                     
                     Console.Write("Insira a linha do navio " + (i + 1) + ": ");
